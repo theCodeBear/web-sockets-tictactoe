e9ed7d5609b7d00df38df6a5b3d0c5d3f229f020
Todd Kronenberg <toddkronenberg@gmail.com>
2015-07-27 21:44:20 -0700

    Made player signup and only two players can play. The rest of the connected people are viewers.

--- client/main.js
+++ client/main.js
@@ -3,6 +3,24 @@ var socket = io();
 var player1 = 'x';
 var player2 = 'o';
 var currentPlayer = player1;
+var username = '';
+
+$('#usernameForm').submit(function() {
+  username = $('#username').val();
+  console.log(username);
+  $('#usernameForm').css('display', 'none');
+  $('#gameboard').css('display', 'initial');
+  socket.emit('player signup', username, function(response) {
+    if (!response.playable) {
+      alert(response.message);
+      $('.square').off('click');
+      $('#userTitle').text('Viewing').css('display', 'block');
+    } else {
+      $('#userTitle').text(username).css('display', 'block');
+    }
+  });
+  return false;
+});
 
 $('.square').on('click', function(el) {
   console.log(el.target.id);
@@ -11,6 +29,15 @@ $('.square').on('click', function(el) {
 
 socket.on('place movement', function(id) {
   $('#'+id).text(currentPlayer);
-  console.log('put place at', id);
   (currentPlayer === player1) ? currentPlayer = player2 : currentPlayer = player1;
+});
+
+socket.on('user connected', function(numOfPlayers) {
+  console.log(numOfPlayers);
+  if (numOfPlayers >= 2) {
+    $('#usernameForm').css('display', 'none');
+    $('#gameboard').css('display', 'initial');
+    $('#userTitle').text('Viewing').css('display', 'block');
+    $('.square').off('click');
+  }
 });
--- server/index.js
+++ server/index.js
@@ -5,9 +5,28 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + './../client'));
 
+var numOfPlayers = 0;
+
 io.on('connection', function(socket) {
-  socket.emit('user connected');
-  console.log('user connected');
+  socket.emit('user connected', numOfPlayers);
+  console.log('user connected', numOfPlayers);
+
+  socket.on('player signup', function(username, cb) {
+    if (numOfPlayers++ >= 2) {
+      numOfPlayers--;
+      cb({
+        message: 'Sorry, both player slots have been filled already.',
+        playable: false
+      });
+    } else {
+      cb({
+        message: 'Hi ' + username + '! You have signed up as player ' + numOfPlayers,
+        playable: true
+      });
+    }
+    console.log(username, 'signed up');
+    cb('you signed up as ' + username);
+  });
 
   socket.on('player moved', function(id) {
     console.log('player touched square', id);
